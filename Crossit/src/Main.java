65299e459cbe94296a3c38e77e7b1fa835dfb02c
figalit <ftaho@yahoo.com>
2016-12-19 17:08:03 +0200

    Thread added to main.

--- Crossit/src/Main.java
+++ Crossit/src/Main.java
@@ -1,7 +1,20 @@
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
+
 
 public class Main extends Frame{
 	
 	public static void main(String[] args) {
-		new Frame();
+		try {
+			SwingUtilities.invokeAndWait(new Runnable(){
+				@Override
+				public void run(){
+					new Frame();
+				}
+			});
+		} catch (InvocationTargetException | InterruptedException e) {
+			// TODO Auto-generated catch block
+		} 
 	}
 }
