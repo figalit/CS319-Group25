6c75d706a3a6951f2e9e7172d2e4358bd779bd96
Utku <Utku@Flare>
2016-12-21 22:15:46 +0200

    test

--- Crossit/src/Main.java
+++ Crossit/src/Main.java
@@ -1,6 +1,16 @@
 public class Main extends Frame{
 	
 	public static void main(String[] args) {
-		Frame.switchPanel(new MainMenu());
+		//Frame.switchPanel(new MainMenu());
+	
+	int[] A ={0,1,0,0,0,1,0,1,0,0}; 
+	int[][] easy = {A,A,A,A,A,A,A,A,A,A};
+	GameGrid grd = new GameGrid(easy,easy,easy);
+	grd.generate(1);
+	System.out.println("Done");
+	grd.print();
+	grd.update();
+	System.out.println('\n');
+	grd.print();
 	}
 }
