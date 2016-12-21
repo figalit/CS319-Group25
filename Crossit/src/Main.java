6f687f82fc38ca4474244f8d1be8d3a012e502ae
Utku <Utku@Flare>
2016-12-21 22:22:59 +0200

    Merge remote-tracking branch 'origin/crossit-general' into crossit-general
    Conflicts:
    	Crossit/src/GameGrid.java
    	Crossit/src/Main.java
    	Crossit/src/Storage.java

--- Crossit/src/Main.java
+++ Crossit/src/Main.java
@@ -1,34 +1,6 @@
-import java.lang.reflect.InvocationTargetException;
-
-import javax.swing.SwingUtilities;
-
 public class Main extends Frame{
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		//Frame.switchPanel(new MainMenu());
-	
-	int[] A ={0,1,0,0,0,1,0,1,0,0}; 
-	int[][] easy = {A,A,A,A,A,A,A,A,A,A};
-	GameGrid grd = new GameGrid(easy,easy,easy);
-	grd.generate(1);
-	System.out.println("Done");
-	grd.print();
-	grd.update();
-	System.out.println('\n');
-	grd.print();
-=======
-		try {
-			SwingUtilities.invokeAndWait(new Runnable(){
-				@Override
-				public void run(){
-					Frame.switchPanel(new MainMenu());
-				}
-			});
-		} catch (InvocationTargetException | InterruptedException e) {
-			// TODO Auto-generated catch block
-		}finally{
-		}
->>>>>>> refs/remotes/origin/crossit-general
+		Frame.switchPanel(new MainMenu());
 	}
 }
