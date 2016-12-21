e88964ff5f4f5540a64341bc953c895b74a24769
Utku <Utku@Flare>
2016-12-21 23:59:47 +0200

    Utku test main

--- Crossit/src/Main.java
+++ Crossit/src/Main.java
@@ -1,6 +1,28 @@
 public class Main extends Frame{
 	
 	public static void main(String[] args) {
-		Frame.switchPanel(new MainMenu());
+		int[] A ={0,1,0,0,0,1,0,1,0,0}; 
+		 	int[][] easy = {A,A,A,A,A,A,A,A,A,A};
+		 	GameGrid grd = new GameGrid(easy,easy,easy);
+		 	grd.generate(1);
+		 	System.out.println("Done");
+		 	grd.print();
+		 	grd.moveCharacter(1);
+		 	grd.update();
+		 	System.out.println('\n');
+		 	grd.print();
+		 	grd.moveCharacter(3);
+		 	//grd.update();
+		 	System.out.println('\n');
+		 	grd.print();
+		 	grd.moveCharacter(2);
+		 	//grd.update();
+		 	System.out.println('\n');
+		 	grd.print();
+		 	grd.moveCharacter(4);
+		 	//grd.update();
+		 	System.out.println('\n');
+		 	grd.print();
 	}
+	
 }
