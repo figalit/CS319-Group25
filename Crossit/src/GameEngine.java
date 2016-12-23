1385163545fb38af8cd09e7205fe1172f963ae1d
figalit <ftaho@yahoo.com>
2016-12-23 23:36:02 +0200

    @Utku: Please review and extend this change.

--- Crossit/src/GameEngine.java
+++ Crossit/src/GameEngine.java
@@ -1,3 +1,5 @@
+import java.util.Timer;
+
 /**
  * @date 19.12.2016
  * @author CrossIt developers!
@@ -17,7 +19,9 @@ public class GameEngine {
 
 	private GameGrid gameGrid;
 	private Storage storage;
-	
+	Timer timer;
+	UpdateGameScheduler scheduler;
+
 	protected GameEngine(){
 		this.stageNo = INIT_NO;
 		this.stageScore = INIT_NO;
@@ -27,9 +31,9 @@ public class GameEngine {
 		this.currentMoney = INIT_NO;
 		this.currentEffect = INIT_NO; // TBD
 		this.storage = new Storage();
-		int[][] a = storage.getVehicleSet(Storage.EASY);
+		//int[][] a = storage.getVehicleSet(Storage.EASY);
 		/*for(int i = 0; i < a.length; i++){
-			for(inst j = 0; j < a[i].length; j++){
+			for(int j = 0; j < a[i].length; j++){
 				System.out.print(a[i][j] + " ");
 			}
 			System.out.println();
@@ -37,7 +41,10 @@ public class GameEngine {
 		this.gameGrid = new GameGrid(storage.getVehicleSet(Storage.EASY),
 									 storage.getVehicleSet(Storage.MED),
 									 storage.getVehicleSet(Storage.HARD));
+		timer = new Timer();
+		scheduler = new UpdateGameScheduler(this);
 	}
+	
 	protected GameEngine(int gameSpeed, int initLifeCount, int currentEffect){
 		this.stageNo = INIT_NO;
 		this.stageScore = INIT_NO;
@@ -46,19 +53,27 @@ public class GameEngine {
 		this.currentLife = initLifeCount;
 		this.currentMoney = INIT_NO;
 		this.currentEffect = currentEffect; // the current effect of the game? 
-		this.storage = new Storage();
-		this.gameGrid = new GameGrid(storage.getVehicleSet(Storage.EASY),
+		//this.storage = new Storage();
+		int[] A ={0,1,0,0,0,1,0,1,0,0}; 
+	    int[][] easy = {A,A,A,A,A,A,A,A,A,A};
+		/*this.gameGrid = new GameGrid(storage.getVehicleSet(Storage.EASY),
 									 storage.getVehicleSet(Storage.MED),
-									 storage.getVehicleSet(Storage.HARD));
+									 storage.getVehicleSet(Storage.HARD));*/
+	    this.gameGrid = new GameGrid(easy, easy, easy);
+		timer = new Timer();
+		scheduler = new UpdateGameScheduler(this);
 	}
 	
 	protected void load(){
-		
+		// do some loading of the screen or maybe some listeners? 
+		timer.schedule(scheduler, 0, 1000);
 	}
 	protected void update(){
-		
+		gameGrid.update();
+		gameGrid.print();
 	}
 	protected boolean checkCollision(){
+		
 		return false; // TODO: change
 	}
 	protected void reduceLife(){
--- Crossit/src/Main.java
+++ Crossit/src/Main.java
@@ -3,31 +3,22 @@ import java.lang.reflect.InvocationTargetException;
 import javax.swing.SwingUtilities;
 
 public class Main extends Frame{
-	
-	public static void main(String[] args) {
-		int[] A ={0,1,0,0,0,1,0,1,0,0}; 
-		 	int[][] easy = {A,A,A,A,A,A,A,A,A,A};
-		 	GameGrid grd = new GameGrid(easy,easy,easy);
-		 	grd.generate(1);
-		 	System.out.println("Done");
-		 	grd.print();
-		 	grd.moveCharacter(1);
-		 	grd.update();
-		 	System.out.println('\n');
-		 	grd.print();
-		 	grd.moveCharacter(3);
-		 	//grd.update();
-		 	System.out.println('\n');
-		 	grd.print();
-		 	grd.moveCharacter(2);
-		 	//grd.update();
-		 	System.out.println('\n');
-		 	grd.print();
-		 	grd.moveCharacter(4);
-		 	//grd.update();
-		 	System.out.println('\n');
-		 	grd.print();
 
+	public static void main(String[] args) {
+		GameEngine engine = new GameEngine();
+		GameGrid grid = engine.getGameGrid();
+		grid.generate(1);
+		try{
+			SwingUtilities.invokeAndWait(new Runnable(){
+				@Override
+				public void run(){
+					//Frame.switchPanel(new MainMenu(grid));
+					GameEngine gameEngine = new GameEngine();
+					gameEngine.load();
+				}
+			});
+		}catch (InvocationTargetException | InterruptedException e){
+			e.getMessage();
+		}
 	}
-	
 }
